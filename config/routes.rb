13ae1b6dbf319eb43b329ce60c02f02d8c2f4770
smplyamzng <kharmon9452@gmail.com>
2019-05-04 12:21:17 -0400

    installations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :contacts
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :students
+  get 'pages/about'
+  root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'Brunelleryan56@gmail.com', password: 'ExpGC9925', password_confirmation: 'ExpGC9925') if Rails.env.development?
