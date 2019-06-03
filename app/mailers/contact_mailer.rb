c212ce87ab84349b2ca8eb225ed4b1dba4024278
smplyamzng <kharmon9452@gmail.com>
2019-06-03 19:40:12 -0400

    mailer set, effects added, social added

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
-	default to: "thronetechnology@gmail.com"
-	# default to: "Expgcapital@gmail.com"
+	# default to: "thronetechnology@gmail.com"
+	default to: "expgcapital@gmail.com"
 
 	def contact_email(first_name, last_name, email, description)
 		@first_name = first_name
